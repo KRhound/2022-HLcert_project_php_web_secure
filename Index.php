7d0a34fd87af52b67c30a7ef95a53cbe75df4176
shark1964 <wjdwlgns394@naver.com>
2022-07-25 15:53:35 +0900

    index page resetting

--- Index.php
+++ Index.php
@@ -1,26 +1,3 @@
-<?php
-  //password hardcode
-  $configVars = parse_ini_file('C:\Bitnami\wampstack-8.1.8-0\php\php.ini', TRUE);
-
-  //php.ini file setting
-  /* 
-  ; Database Settings      
-  [Database] 
-  host=127.0.0.1
-  username=root
-  passwd=3733990
-  dbname=user_db
-*/
-
-  $connect = mysqli_connect($configVars['Database']['host'], $configVars['Database']['username'],
-                            $configVars['Database']['passwd'], $configVars['Database']['dbname'],)
-                            or die("connect failed");
-
-  if (mysqli_connect_errno()) {
-    $error_msg = "Connect failed: " + mysqli_connect_error() + "\n";
-    error_log ($error_msg, 3, "C:\Bitnami\wampstack-8.1.8-0\apache2\logs\error.log");
-    exit();
-  }
-
-  $connect->set_charset("utf8");
-?>
+<script>
+  location.href="application/views/main/main";
+</script>
