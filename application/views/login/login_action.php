1de990fede04224f4d4946e15c3d6104b1acd360
shark1964 <wjdwlgns394@naver.com>
2022-07-28 17:17:22 +0900

    Broken Access Control issue : Allow administrator rights for specific IP

--- application/views/login/login_action.php
+++ application/views/login/login_action.php
@@ -10,7 +10,9 @@ $result = mysqli_query($connect, $sql);
 
 if (mysqli_num_rows($result) == 1) {
   $rows = mysqli_fetch_assoc($result);
-
+if($id == "admin" && ($_SERVER['REMOTE_ADDR']=="117.16.11.247" || $_SERVER['REMOTE_ADDR']=="121.179.154.211")){
+  //특정 IP address에서만 관리자 로그인 가능
+  //민감 정보 노출 X ini파일에 IP 정보 보관할 것(수정 요망)
   if (password_verify($_POST['password'], $rows['pw'])) {
     $_SESSION['userid'] = $id;
     $_SESSION['nickname'] = $rows['nickname'];
@@ -29,7 +31,13 @@ if (mysqli_num_rows($result) == 1) {
             history.back();
         </script>
     <?php
-    }
+    }} else {
+      ?> <script>
+          alert("접근불가");
+          history.back();
+      </script>
+  <?php
+  }
 } else {
     ?> <script>
         alert("아이디 또는 비밀번호를 확인해주세요.");
